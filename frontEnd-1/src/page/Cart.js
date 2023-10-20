3a7752f8abc1cf3917588b1b7ede4fcf07b6faea
ChaudharySB <142977514+ChaudharySB@users.noreply.github.com>
2023-10-20 16:28:48 +0530

    Cart.js

--- frontEnd-1/src/page/Cart.js
+++ frontEnd-1/src/page/Cart.js
@@ -33,8 +33,8 @@ const Cart = () => {
     0
   );
 
-  
-  
+
+
   const handlePayment = async()=>{
 
       if(user.email){
@@ -54,7 +54,7 @@ const Cart = () => {
           console.log(data)
 
           toast("Redirect to payment Gateway...!")
-          stripePromise.redirectToCheckout({sessionId : data}) 
+          stripePromise.redirectToCheckout({sessionId : data})
       }
       else{
         toast("You have not Login!")
@@ -62,67 +62,56 @@ const Cart = () => {
           navigate("/login")
         },1000)
       }
-    
+
   }
   return (
-    <>
-    
       <div className="p-2 md:p-4">
-        <h2 className="text-lg md:text-2xl font-bold text-slate-600 ">
-          Your Cart Items:-
-        </h2>
+          <h2 className="text-lg md:text-2xl font-bold text-slate-600">Your Cart Items:-</h2>
 
+          {productCartItem[0] ? (
+              <div className="my-4 flex flex-col md:flex-row gap-3">
+                  {/* display cart items  */}
+                  <div className="w-full max-w-3xl md:w-1/2">
+                      {productCartItem.map((el) => (
+                          <CartProduct
+                              key={el._id}
+                              id={el._id}
+                              name={el.name}
+                              image={el.image}
+                              category={el.category}
+                              qty={el.qty}
+                              total={el.total}
+                              price={el.price}
+                          />
+                      ))}
+                  </div>
 
-        {productCartItem[0] ?
-        <div className="my-4 flex gap-3">
-          {/* display cart items  */}
-          <div className="w-full max-w-3xl ">
-            {productCartItem.map((el) => {
-              return (
-                <CartProduct
-                  key={el._id}
-                  id={el._id}
-                  name={el.name}
-                  image={el.image}
-                  category={el.category}
-                  qty={el.qty}
-                  total={el.total}
-                  price={el.price}
-                />
-              );
-            })}
-          </div>
-
-          {/* total cart item  */}
-          <div className="w-full max-w-md  ml-auto">
-            <h2 className="bg-blue-500 text-white p-2 text-lg">Summary</h2>
-            <div className="flex w-full py-2 text-lg border-b">
-              <p>Total Qty :</p>
-              <p className="ml-auto w-32 font-bold">{totalQty}</p>
-            </div>
-            <div className="flex w-full py-2 text-lg border-b">
-              <p>Total Price</p>
-              <p className="ml-auto w-32 font-bold">
-                <span className="text-red-500">₹</span> {totalPrice}
-              </p>
-            </div>
-            <button className="bg-red-500 w-full text-lg font-bold py-2 text-white" onClick={handlePayment}>
-              Payment
-            </button>
-          </div>
-        </div>
-
-        : 
-        <>
-          <div className="flex w-full justify-center items-center flex-col">
-            <img src={emptyCartImage} className="w-50 max-w-sm"/>
-            <p className="text-red-400 text-3xl font-bold">Your cart is EMPTY!!</p>
-          </div>
-        </>
-      }
+                  {/* total cart item  */}
+                  <div className="w-full max-w-md md:w-1/2 ml-auto">
+                      <h2 className="bg-blue-500 text-white p-2 text-lg">Summary</h2>
+                      <div className="flex w-full py-2 text-lg border-b">
+                          <p>Total Qty:</p>
+                          <p className="ml-auto w-32 font-bold">{totalQty}</p>
+                      </div>
+                      <div className="flex w-full py-2 text-lg border-b">
+                          <p>Total Price</p>
+                          <p className="ml-auto w-32 font-bold">
+                              <span className="text-red-500">₹</span> {totalPrice}
+                          </p>
+                      </div>
+                      <button className="bg-red-500 w-full text-lg font-bold py-2 text-white" onClick={handlePayment}>
+                          Payment
+                      </button>
+                  </div>
+              </div>
+          ) : (
+              <div className="flex flex-col items-center">
+                  <img src={emptyCartImage} className="w-50 max-w-sm" alt="Empty Cart" />
+                  <p className="text-red-400 text-3xl font-bold">Your cart is EMPTY!!</p>
+              </div>
+          )}
       </div>
-    
-    </>
+
   );
 };
 
